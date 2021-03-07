ea8fd665b720dbe5e46ee46307f3cdc5b4bb9561
Alquipo <alquiponeto@outlook.com.br>
2021-03-07 15:12:56 -0300

    feat: places page

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/dist/client/router'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import * as S from './styles'
 
@@ -13,32 +14,41 @@ type Place = {
 export type MapProps = {
   places?: Place[]
 }
-const Map = ({ places }: MapProps) => (
-  <S.MapWrapper>
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      style={{ height: '100%', width: '100%' }}
-      minZoom={3}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+const Map = ({ places }: MapProps) => {
+  const router = useRouter()
 
-      {places?.map(({ id, name, location }) => {
-        const { longitude, latitude } = location
+  return (
+    <S.MapWrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        style={{ height: '100%', width: '100%' }}
+        minZoom={3}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-        return (
-          <Marker
-            key={`place-${id}`}
-            position={[latitude, longitude]}
-            title={name}
-          />
-        )
-      })}
-    </MapContainer>
-  </S.MapWrapper>
-)
+        {places?.map(({ id, slug, name, location }) => {
+          const { longitude, latitude } = location
+
+          return (
+            <Marker
+              key={`place-${id}`}
+              position={[latitude, longitude]}
+              title={name}
+              eventHandlers={{
+                click: () => {
+                  router.push(`/place/${slug}`)
+                }
+              }}
+            />
+          )
+        })}
+      </MapContainer>
+    </S.MapWrapper>
+  )
+}
 
 export default Map
---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,29 @@
+type ImageProps = {
+  url: string
+  height: string
+  width: string
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+      {console.log(place.gallery)}
+      {place.gallery.map((image, index) => (
+        <img key={`photo-${index}`} src={image.url} alt={place.name} />
+      ))}
+    </>
+  )
+}
