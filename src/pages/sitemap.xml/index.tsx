9b9df243281f3f970e9577c8815733f4571d1e09
Alquipo <alquiponeto@outlook.com.br>
2021-08-19 16:43:33 -0300

    fix: sitemap

---
+++ src/pages/sitemap.xml/index.tsx
@@ -0,0 +1,31 @@
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+import client from 'graphql/client'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES } from 'graphql/queries'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+
+  const fields = places.map(({ slug }) => ({
+    loc: `https://my-trips.alquipo.dev/${slug}`,
+    lastmod: new Date().toISOString()
+  }))
+
+  fields.push(
+    {
+      loc: 'https://my-trips.alquipo.dev',
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: 'https://my-trips.alquipo.dev/about',
+      lastmod: new Date().toISOString()
+    }
+  )
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default () => {}
